6257cc4cb97a59335eb0d6fa20346d32adeb2587
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-08-17 23:58:00 -0300

    managed rules

--- modules/dns-firewall/main.tf
+++ modules/dns-firewall/main.tf
@@ -30,11 +30,47 @@ resource "aws_route53_resolver_firewall_rule" "block_custom" {
   priority                = 100
 }
 
-# TODO: Add managed rule
-
 resource "aws_route53_resolver_firewall_rule_group_association" "my_group" {
   name                   = "mygroup-association"
   firewall_rule_group_id = aws_route53_resolver_firewall_rule_group.my_group.id
   priority               = 200
   vpc_id                 = var.vpc_id
+}
+
+### Managed Rules (us-east-2) ###
+
+resource "aws_route53_resolver_firewall_rule" "AWSManagedDomainsAggregateThreatList" {
+  name                    = "AWSManagedDomainsAggregateThreatList"
+  action                  = "BLOCK"
+  block_response          = "NODATA"
+  firewall_domain_list_id = "rslvr-fdl-bbc798062d594728"
+  firewall_rule_group_id  = aws_route53_resolver_firewall_rule_group.my_group.id
+  priority                = 101
+}
+
+resource "aws_route53_resolver_firewall_rule" "AWSManagedDomainsAmazonGuardDutyThreatList" {
+  name                    = "AWSManagedDomainsAmazonGuardDutyThreatList"
+  action                  = "BLOCK"
+  block_response          = "NODATA"
+  firewall_domain_list_id = "rslvr-fdl-996a2e1ce1314abb"
+  firewall_rule_group_id  = aws_route53_resolver_firewall_rule_group.my_group.id
+  priority                = 102
+}
+
+resource "aws_route53_resolver_firewall_rule" "AWSManagedDomainsBotnetCommandandControl" {
+  name                    = "AWSManagedDomainsBotnetCommandandControl"
+  action                  = "BLOCK"
+  block_response          = "NODATA"
+  firewall_domain_list_id = "rslvr-fdl-3c30dcb4c50b4401"
+  firewall_rule_group_id  = aws_route53_resolver_firewall_rule_group.my_group.id
+  priority                = 103
+}
+
+resource "aws_route53_resolver_firewall_rule" "AWSManagedDomainsMalwareDomainList" {
+  name                    = "AWSManagedDomainsMalwareDomainList"
+  action                  = "BLOCK"
+  block_response          = "NODATA"
+  firewall_domain_list_id = "rslvr-fdl-19f9b4c730a14748"
+  firewall_rule_group_id  = aws_route53_resolver_firewall_rule_group.my_group.id
+  priority                = 104
 }
